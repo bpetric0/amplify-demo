09df328e4e6ec5d7accce8205ca8788283cd3f9c
petricb <branko.petric@gildedco.com>
2022-09-29 01:42:30 +0200

    Removed build specs and added new env

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,10 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "testamplify6ad70618": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
